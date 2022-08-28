32071101abe7499502cb55181db0da2b5ef20d99
pawelkom88e <pawel.komorkiewicz@yahoo.co.uk>
2022-08-28 20:29:01 +0100

    chore: Add comment

--- src/js/index.js
+++ src/js/index.js
@@ -2,19 +2,24 @@ const ctaBtn = document.querySelector(".cta-btn");
 const modal = document.querySelector(".modal");
 const overlay = document.querySelector(".overlay");
 
+// open modal
 ctaBtn.addEventListener("click", () => {
   modal.classList.toggle("modal-active");
   overlay.classList.add("modal-active");
 });
 
+// close modal after clicking on overlay
 overlay.addEventListener("click", () => {
   modal.classList.remove("modal-active");
   overlay.classList.remove("modal-active");
 });
 
-document.addEventListener("keydown", function (event) {
-  if (event.key === "Escape") {
+// close modal using keyboard
+document.addEventListener("keydown", e => {
+  if (e.key === "Escape") {
     modal.classList.remove("modal-active");
     overlay.classList.remove("modal-active");
   }
 });
+
+// aria expanded after clicking hamburger true
